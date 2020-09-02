39d5375cf44d71ad33bf6fc2e94befe4fbd1d175
George Albuquerque Pinto <georgealbuquerquepinto@hotmail.com>
2020-09-02 13:16:14 -0300

    Add data

--- src/_utils/data.js
+++ src/_utils/data.js
@@ -1,20 +1,32 @@
 export default [
   {
     id: '1',
-    name: 'Pacote redes sociais 1',
-    description: 'Gerenciamento de redes sociais com postagens semanais',
+    name: 'Item 1',
+    description: 'Item 1 description',
     value: 99.99,
   },
   {
     id: '2',
-    name: 'Pacote redes sociais 2',
-    description: 'Gerenciamento de redes sociais com 3 postagens semanais',
+    name: 'Item 2',
+    description: 'Item 2 description',
     value: 149.99,
   },
   {
     id: '3',
-    name: 'Pacote redes sociais 3',
-    description: 'Gerenciamento de redes sociais com postagens diárias',
+    name: 'Item 3',
+    description: 'Item 3 description',
     value: 199.99,
+  },
+  {
+    id: '4',
+    name: 'Item 4',
+    description: 'Item 4 description',
+    value: 249.99,
+  },
+  {
+    id: '5',
+    name: 'Item 5',
+    description: 'Item 5 description',
+    value: 299.99,
   },
 ];
